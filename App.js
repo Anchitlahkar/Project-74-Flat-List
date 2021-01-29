7e079b2e7eae5adea7e28e5df4ddb3b9531c36cb
Anchit <anchitlahkar0202@gmail.com>
2021-01-29 13:23:43 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import writeStoryScreen from './screens/WriteStory';
+import readStoryScreen from './screens/ReadStory';
+import ReadStory from './screens/Story';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Write_Story: {
+      screen: writeStoryScreen,
+    },
+    Read_Story: {
+      screen: readStoryScreen,
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const routeName = navigation.state.routeName;
+        if (routeName === 'Write_Story') {
+          return (
+            <Image
+              style={{ height: 32, width: 40 }}
+              source={require('./assets/write.png')}
+            />
+          );
+        } else if (routeName === 'Read_Story') {
+          return (
+            <Image
+              style={{ height: 32, width: 32 }}
+              source={require('./assets/read.png')}
+            />
+          );
+        }
+      },
+    }),
+  }
+);
+
+var AppNavigator = createSwitchNavigator({
+  TabNavigator: TabNavigator,
+  Story: ReadStory,
+});
+
+const AppContainer = createAppContainer(AppNavigator);
